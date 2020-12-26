08dd19d2706560846119deef45bc32938a43ea76
Dltmd202 <dltmd202@gmail.com>
2020-12-26 09:40:09 +0900

    Request Web Server from Http

---
+++ SampleHttp/app/src/main/java/com/example/samplehttp/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.samplehttp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
+    Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+//http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101
+//http://newsapi.org/v2/top-headlines?country=kr&apiKey=bfda38d75b454d64be66a2d01f55f552
+//http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String url ="http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101";
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request(url);
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public void request(String urlStr){
+        StringBuilder output = new StringBuilder();
+        try{
+            URL url = new URL(urlStr);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            if(conn != null){
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+
+                int resCode = conn.getResponseCode();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = null;
+                while(true){
+                    line = reader.readLine();
+                    if(line == null){
+                        break;
+                    }
+                    output.append(line+'\n');
+                }
+                reader.close();
+                conn.disconnect();
+            }
+
+
+
+
+
+        }catch(Exception e) { e.printStackTrace(); }
+
+        println(output.toString());
+    }
+
+    public void println(final String data){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.append(data+'\n');
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
