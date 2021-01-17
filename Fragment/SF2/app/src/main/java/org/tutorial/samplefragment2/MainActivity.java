c3f8e9c2fe3404f3bee136d51f7c478dd42c1e04
Dltmd202 <dltmd202@gmail.com>
2021-01-18 07:48:18 +0900

    List with Fragment

---
+++ Fragment/SF2/app/src/main/java/org/tutorial/samplefragment2/MainActivity.java
@@ -0,0 +1,27 @@
+package org.tutorial.samplefragment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements ListFragment.ImageSelectionCallback{
+    int[] images = {R.drawable.profile1 , R.drawable.image , R.drawable.image2};
+
+    ListFragment listFragment;
+    ViewFragment viewFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager manager= (FragmentManager)getSupportFragmentManager();
+        listFragment = (ListFragment)manager.findFragmentById(R.id.listFragment);
+        viewFragment = (ViewFragment)manager.findFragmentById(R.id.viewFragment);
+
+    }
+
+    @Override
+    public void onImageSelected(int position) {
+        viewFragment.setImage(images[position]);
+    }
+}c
